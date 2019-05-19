7f1bb4725ba52c9c492984978f0d0a3c23beabdf
Thomas Kaiser <thomas1990kaiser@gmail.com>
2019-05-19 14:32:43 +0200

    improve slick

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     $('.slider').not('.slick-initialized').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToScroll: 1,
         arrows: true,
         centerMode: true,
         variableWidth: true,
