5f0e16ddb0e34509b95a15f91df0475e3a5f4c90
Thomas Kaiser <thomas1990kaiser@gmail.com>
2019-06-09 13:08:53 +0200

    styling :wq

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
 $(document).ready(function () {
     $('.slider').not('.slick-initialized').slick({
         infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
+        slidesToShow: 2,
+        slidesToScroll: 2,
         arrows: true,
         centerMode: true,
         variableWidth: true,
         dots: true,
+        // responsive: [
+        //     {
+        //         breakpoint: 480,
+        //         settings: {
+        //             slidesToShow: 1,
+        //             slidesToScroll: 1,
+        //             infinite: true,
+        //             dots: true
+        //         }
+        //     }
+        // ]
     });
 });
