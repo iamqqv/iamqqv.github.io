74386f55d458fe6534b904b1ec40f39be5a724cd
tkaiser <TKaiser@chip.de>
2020-10-22 09:37:42 +0200

    slider arrows and remove container padding

--- js/app.js
+++ js/app.js
@@ -2,10 +2,12 @@ $(document).ready(function () {
     $('.slider').not('.slick-initialized').slick({
         infinite: true,
         slidesToShow: 1,
-        arrows: true,
         centerMode: true,
         variableWidth: true,
         dots: true,
+        arrows: true,
+        // prevArrow: "<img class='prevArrow' src='../assets/img/slider-arrow.jpg'>",
+        // nextArrow: "<img class='nextArrow' src='../assets/img/slider-arrow.jpg'>"
     });
 });
 
